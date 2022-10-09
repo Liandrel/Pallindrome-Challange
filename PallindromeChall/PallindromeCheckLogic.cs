37dc689793e80b86aaaaa12a664bdd8aa13b7a6a
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-09 12:54:00 +0200

    Pallindrome Check Logic
    Created pallindrome check logic and function to remove all special characters

--- PallindromeChall/PallindromeCheckLogic.cs
+++ PallindromeChall/PallindromeCheckLogic.cs
@@ -8,5 +8,40 @@ namespace PallindromeChall
 {
     public static class PallindromeCheckLogic
     {
+        public static bool IsAPallindrome<T>(T input)
+        {
+            bool output = false;
+
+            string normal = input.ToString();
+            string reversed = "";
+
+            for (int i = normal.Length - 1; i > -1; i--)
+            {
+                reversed += normal[i];
+            }
+
+            normal = normal.RemoveSpecialCharacters();
+            reversed = reversed.RemoveSpecialCharacters();
+
+            if (normal == reversed)
+            {
+                output = true;
+            }
+
+            return output;
+        }
+        private static string RemoveSpecialCharacters(this string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in str)
+            {
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString().ToLower();
+        }
     }
+
 }
